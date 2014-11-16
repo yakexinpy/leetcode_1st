0275cf591a03c97ee2b7bf191797bb64600fc0bc
yakexinpy <yakexittmtt@gmail.com>
2014-11-15 22:48:15 -0500

    Create Maximum Depth of Binary Tree.java

---
+++ Maximum Depth of Binary Tree.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int max = Math.max(maxDepth(root.left), maxDepth(root.right));
+        max += 1;
+        return max;
+    }
+}
