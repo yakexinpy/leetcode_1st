ee7af387d01714623bbd0b7d09b25c609a8a2d46
yakexinpy <yakexittmtt@gmail.com>
2014-11-15 23:28:52 -0500

    Create Balanced Binary Tree.java

---
+++ Balanced Binary Tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    class Result{
+        boolean res;
+        int depth;
+        
+        public Result(boolean b, int d){
+            res = b;
+            depth = d;
+        }
+    }
+    
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)
+            return true;
+        Result res = isBalancedHelper(root);
+        return res.res;
+    }
+    
+    private Result isBalancedHelper(TreeNode root){
+        if(root == null)
+            return new Result(true, 0);
+        Result l = isBalancedHelper(root.left);
+        if(!l.res){
+                return new Result(false,0);
+        }
+        Result r = isBalancedHelper(root.right);
+        if(!r.res){
+                return new Result(false,0);
+        }
+        int dep = Math.abs(l.depth - r.depth);
+        boolean b = (dep <= 1);
+        return new Result(b, Math.max(l.depth, r.depth) + 1);
+    }
+    
+}
