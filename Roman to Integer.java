8a0b06fa7fb10f5a6d227d2a7b74acfeeb2e1a20
yakexinpy <yakexittmtt@gmail.com>
2014-11-16 19:55:22 -0500

    Create Roman to Integer.java

---
+++ Roman to Integer.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public int romanToInt(String s) {
+        if(s == null && s == "")
+            return 0;
+        
+        int sum = value(s.charAt(0));
+        int pre, cur;
+        for(int i = 1; i < s.length(); i++){
+            cur = value(s.charAt(i));
+            pre = value(s.charAt(i - 1));
+            if(cur <= pre){
+                sum += cur;
+            }
+            else{
+                sum = sum - pre * 2 + cur;
+            }
+        }
+        return sum;
+    }
+    
+    private int value(char ch){
+        switch(ch){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default: 
+                return 0;
+        }
+    }
+}
