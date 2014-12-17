086ae608f2015d39d52e2e624baea59c7b46d421
yakexinpy <yakexittmtt@gmail.com>
2014-12-16 20:52:43 -0500

    Create Binary Tree Preorder Traversal.java

---
+++ Binary Tree Preorder Traversal.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null)
+            return result;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode temp = stack.pop();
+            result.add(temp.val);
+            if(temp.right != null)
+                stack.push(temp.right);
+            if(temp.left != null)
+                stack.push(temp.left);
+        }
+        return result;
+    }
+}
