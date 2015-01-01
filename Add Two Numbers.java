a92da9e2fdaaa9547c7d655b759a9c3fb63cc7ec
yakexinpy <yakexittmtt@gmail.com>
2015-01-01 11:35:24 -0800

    Create Add Two Numbers.java

---
+++ Add Two Numbers.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null || l2 == null) return (l1 == null) ? l2 : l1;
+        ListNode H = new ListNode(-1);
+        ListNode cur = H;
+        int res = 0;
+        int sum = 0;
+        while(l1 != null || l2 != null){
+            sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + res;
+            res = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            l1 = (l1 != null) ? l1.next : l1;
+            l2 = (l2 != null) ? l2.next : l2;
+        }
+        if(res != 0)
+            cur.next = new ListNode(res);
+        return H.next;
+    }
+}
